49eb793cb7627bcf636182fc272d14ce8bc0ad45
jcornellasg <joan.cornellasg@educem.net>
2022-11-11 14:20:53 +0100

    -

--- src/app/homeclientes/home.page.ts
+++ src/app/homeclientes/home.page.ts
@@ -9,4 +9,22 @@ export class HomePage {
 
   constructor() {}
 
+
+  clickSegment(pagina) {
+    //console.log(pagina);
+    var pagina1 = document.getElementById("pagina1");
+    var pagina2 = document.getElementById("pagina2");
+
+    if (pagina == 1) {
+      //console.log("reservas");
+
+      pagina1.style.display = "block";
+      pagina2.style.display = "none";
+    } else if (pagina == 2) {
+      //console.log("restaurantes");
+
+      pagina1.style.display = "none";
+      pagina2.style.display = "block";
+    }
+  }
 }
